0211654bd6747e422780b0535ed2bab978f8f8fd
Jonas Schöley <jschoeley@gmail.com>
2017-05-15 22:55:21 +0200

    Update Shape Measures Vignette

--- vignettes/shape_measures_and_their_implementation.R
+++ vignettes/shape_measures_and_their_implementation.R
@@ -3,25 +3,28 @@ options(width = 100)
 knitr::opts_chunk$set(tidy = FALSE, size = "small")
 
 ## -------------------------------------------------------------------------------------------------
-# Average Years of Life Lost due to Death in Age x
-eDaggerx <- function(nax, nx, ex) {
+# Average Life-Expectancy in Age x
+EDaggerx <- function(nax, nx, ex) {
   nAx = nax/nx
-  edx = (nAx * c(ex[-1L], 0) + (1 - nAx) * ex)
+  edx = (nAx*c(ex[-1L], 0) + (1-nAx)*ex)
+  edx[length(edx)] = ex[length(ex)]
   return(edx)
 }
 
 # Total Life Years Lost due to Death
 EDagger <- function(nax, nx, ndx, ex) {
-  edx = eDaggerx(nax, nx, ex)
+  edx = EDaggerx(nax, nx, ex)
   ed  = sum(ndx*edx)
   return(ed)
 }
 
 # Life Table Entropy
-LifetableEntropy <- function(nax, nx, ndx, ex) {
+LifetableEntropy <- function(nax, nx, ndx, ex, harmonized) {
   ed = EDagger(nax, nx, ndx, ex)
-  H  = 1 - ed / ex[1L]
-  return(H)
+  H  = ed/ex[1L]
+  if (!isTRUE(harmonized)) {S = H}
+  if (isTRUE(harmonized)) {S = 1-H}
+  return(S)
 }
 
 ## ----eval=FALSE-----------------------------------------------------------------------------------
@@ -41,72 +44,56 @@ LifetableEntropy <- function(nax, nx, ndx, ex) {
 #  }
 
 ## -------------------------------------------------------------------------------------------------
-LifetableGini <- function(nax, nx, lx, ex) {
-  nAx <- nax/nx
-  Gx = nAx * c(lx[-1L], 0) + (1-nAx)*lx
-  Gx = Gx^2 * nx
-  G  = 1 - 1/ex[1L]*sum(Gx)
-  return(G)
+# Life Table Gini-Coefficient
+LifetableGini <- function (x, nax, ndx, ex, harmonized) {
+  e = rep(1, length(x))
+  D = outer(ndx, ndx)
+  x_ = x+nax
+  X_ = abs(e%*%t(x_) - x_%*%t(e))
+  G = sum(D*X_)/(2*ex[1L])
+  if (!isTRUE(harmonized)) {S = G}
+  if (isTRUE(harmonized)) {S = 1-2*G}
+  return(S)
 }
 
 ## -------------------------------------------------------------------------------------------------
-LifetableGini2 <- function(nax, nx, lx, ex) {
-  nAx  = nax/nx
-  lx_1 = c(lx[-1L], 0)
-  Gx   = lx_1^2 + nAx*(lx^2 - lx_1^2)
-  Gx   = Gx*nx
-  G    = 1 - 1/ex[1L] * sum(Gx)
-  return(G)
-}
-
-## -------------------------------------------------------------------------------------------------
-LifetableGini3 <- function(nax, nx, lx, ex, ndx) {
-  lx_1 = c(lx[-1L], 0)
-  Gx   = (lx^2 + lx_1^2)/2 - (ndx^2)/6
-  Gx   = Gx*nx
-  G    = 1 - 1/ex[1L]*sum(Gx)
-  return(G)
-}
-
-## -------------------------------------------------------------------------------------------------
-MortalityRatio <- function(x, nx, nmx, ex){
+# Mortality Ratio
+MortalityRatio <- function(x, nx, nmx, ex, harmonized){
   m0   = nmx[1L]
-  m_e0 = FindValue(measure = nmx, x, nx, ex)
-  MR   = 1 - m0/m_e0
-  return(MR)
+  m_e0 = approx(x = x, y = nmx, xout = ex[1L])[["y"]]
+  MR   = m0/m_e0
+  if (!isTRUE(harmonized)) {S = MR}
+  if (isTRUE(harmonized)) {S = 1 - MR}
+  return(S)
 }
 
-FindValue <- function(measure, x, nx, ex){
-  # I know this looks crazy!
-  e0     = ex[1L]
-  e0_    = max(x[(x - e0) <= 0])       # find the floor of e0 value
-  m_e0_  = measure[x == e0_]           # find the floor of nmx (when x = e0_)
-  n_e0_  = nx[x == e0_]
-  m_e0up = measure[x == (e0_ + n_e0_)] # find the ceiling of nmx (when x = e0_ + nx)
-  E0     = e0 - e0_                    # frac(e0)
-  M_e0   = (m_e0_*(n_e0_ - E0) + m_e0up * E0) / n_e0_
-  return(M_e0)
+
+## -------------------------------------------------------------------------------------------------
+# Life Expectancy Ratio
+LER <- function(x, nx, ex, harmonized){
+  e_e0 = approx(x = x, y = ex, xout = ex[1L])[["y"]]
+  ler = e_e0/ex[1L]
+  if (!isTRUE(harmonized)) {S = ler}
+  if (isTRUE(harmonized)) {S = 1-ler}
+  return(S)
 }
 
 ## -------------------------------------------------------------------------------------------------
-LER <- function(x, nx, ex){
-  e0   = ex[1L]
-  e_e0 = FindValue(measure = ex, x, nx, ex)
-  ler  =  1 - e_e0/e0
-  return(ler)
-}
-
-## -------------------------------------------------------------------------------------------------
-ACFM <- function(nmx, ndx, ex){
+# Average of Change in Force of Mortality with respect to lx
+ACFM <- function(nmx, ndx, ex, harmonized){
   acfm_x = (nmx - nmx[1L]) * ndx
-  acfm   = 1 - exp(-ex[1L] * sum(acfm_x))
-  return(acfm)
+  D = ex[1L] * sum(acfm_x)
+  if (!isTRUE(harmonized)) {S = D}
+  if (isTRUE(harmonized)) {S = 1-exp(-D)}
+  return(S)
 }
 
 ## -------------------------------------------------------------------------------------------------
-PSMAD <- function(x, nx, lx, ex){
-  l_e0  = FindValue(measure = lx, x, nx, ex)
-  psmad = 1 - log(l_e0)
-  return(psmad)
+# Probability to Survive up to the Mean Age at Death
+PSMAD <- function(x, nx, lx, ex, harmonized){
+  l_e0  = approx(x = x, y = lx, xout = ex[1L])[["y"]]
+  if (!isTRUE(harmonized)) {S = l_e0}
+  if (isTRUE(harmonized)) {S = 1 + log(l_e0)}
+  return(S)
 }
 
