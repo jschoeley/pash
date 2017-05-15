0d96e9c4d0c4ed98757f4d9362841f67489fe489
Jonas Schöley <jschoeley@gmail.com>
2017-05-15 11:37:57 +0200

    Rename CDR To Life-table Death Rate

--- R/pace_measures.R
+++ R/pace_measures.R
@@ -12,7 +12,7 @@
 #' \describe{
 #'   \item{\code{"e0"}}{Total life expectancy.}
 #'   \item{\code{"qlx"}}{Age where q percent of the life table population is still alive.}
-#'   \item{\code{"cdr"}}{The average mortality rate over all ages (crude death rate).}
+#'   \item{\code{"ldr"}}{The life-table death rate.}
 #'   \item{\code{"all"}}{All of the above measures}
 #' }
 #'
@@ -50,12 +50,12 @@ GetPace <- function (pash, type = "all", q = 0.5) {
     S = c(qlx = SurvivalQuantile(lt[["x"]], lt[["nx"]], lt[["lx"]], q,
                                  nax_mode = attr(pash, "nax_mode")))
   }
-  if (identical(type, "cdr")) {S = c(cdr = CDR(lt[["ex"]]))}
+  if (identical(type, "ldr")) {S = c(ldr = LDR(lt[["ex"]]))}
   if (identical(type, "all")) {
     S = c(e0 =  TotalLifeExpectancy(lt[["ex"]]),
           qlx = SurvivalQuantile(lt[["x"]], lt[["nx"]], lt[["lx"]], q,
                                  nax_mode = attr(pash, "nax_mode")),
-          cdr = CDR(lt[["ex"]]))
+          ldr = LDR(lt[["ex"]]))
   }
   return(S)
 }
@@ -67,17 +67,17 @@ TotalLifeExpectancy <- function (ex) {
   return(ex[1L])
 }
 
-#' Crude Death Rate
+#' Life-table Death Rate
 #'
-#' @details The Crude Death Rate is the inverse of the total life-expectancy. It
-#'   can also be understood as the weighted harmonic mean of the force of
-#'   mortality, i.e. mu-bar.
+#' @details The Life-table death rate is the inverse of the total
+#'   life-expectancy. It can also be understood as the weighted harmonic mean of
+#'   the force of mortality, i.e. mu-bar.
 #'
 #' @source Wrycza, Tomasz, and Annette Baudisch. 2014.
 #' "The Pace of Aging: Intrinsic Time Scales in Demography."
 #' Demographic Research 30 (1): 1571-90. doi:10.4054/DemRes.2014.30.57.
 #' @keywords internal
-CDR <- function (ex) { return(1/TotalLifeExpectancy(ex)) }
+LDR <- function (ex) { return(1/TotalLifeExpectancy(ex)) }
 
 # Survival Quantile
 SurvivalQuantile <- function (x, nx, lx, q, nax_mode) {
